1ab46089e945af6124d91e56e371cc7ec961c995
Gustavo Fontana <gustavoasf77@gmail.com>
2026-02-03 22:40:14 -0300

    init spring

---
+++ src/test/java/gh/gustass/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gh.gustass.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
